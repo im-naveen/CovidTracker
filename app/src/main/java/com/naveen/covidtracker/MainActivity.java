8f298f38b16dd305f807ad6c0cb990f301742966
Naveen <naveenannaih08@gmail.com>
2021-05-17 20:50:46 +0530

    Added progress bar

--- app/src/main/java/com/naveen/covidtracker/MainActivity.java
+++ app/src/main/java/com/naveen/covidtracker/MainActivity.java
@@ -3,8 +3,12 @@ package com.naveen.covidtracker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView textview_confirmed, textview_confirmed_new, textview_active, textview_active_new, textview_recovered, textview_recovered_new, textview_death,
+            textview_death_new, textview_tests, textview_tests_new, textview_date, textview_time;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
